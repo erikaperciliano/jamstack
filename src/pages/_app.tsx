b33e80f163e4e7daba8223cc118bd72ffc16024a
Erika Perciliano <erikaperciliano.developer@gmail.com>
2022-02-22 12:46:57 -0300

    removing comments

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,6 @@ import { client, ssrCache } from '../lib/urql'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  //se já tiver feito um query pelo lado do server side da app e estiver utilizando os mesmos dados dessa query
-  //no cliente side, não será feito uma query de novo
     if(pageProps.urlState) {
       ssrCache.restoreData(pageProps.urqlState);
     }
